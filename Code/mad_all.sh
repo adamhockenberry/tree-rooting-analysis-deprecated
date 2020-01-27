46cbb854002424884711bb81b4e58f514e426c1d
adamhockenberry <hockenberry.adam@gmail.com>
2020-01-27 13:52:24 -0600

    pushing from workstation

--- Code/mad_all.sh
+++ Code/mad_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#for filename in ../Tria_et_al_data/eukaryotes/ingroup/phyml/*.nwk; do
-for filename in ../Tria_et_al_data/eukaryotes/ingroup/phyml/*.pruned; do
-    ~/workspace/mad/mad.osx $filename
+#for filename in ../Data/Tria_et_al_data/eukaryotes/ingroup/phyml/*.nwk; do
+for filename in ../Data/Tria_et_al_data/eukaryotes/pruned_trees/*.MADFORMAT; do
+#for filename in ../Data/OMA_group_data/eukaryotes/raw_OMA_trees/*.treefile; do
+#for filename in ../Tria_et_al_data/eukaryotes/ingroup/phyml/*.pruned; do
+    ~/Workspace/mad/mad $filename
     wait
 done
 
